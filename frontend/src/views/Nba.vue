36cb7c474f30ba532fa4bffbba4d05f405ad15b8
root <taekg.park@gmail.com>
2022-06-10 20:50:13 +0900

    init

---
+++ frontend/src/views/Nba.vue
@@ -0,0 +1,16 @@
+<template>
+  <navbar />
+  
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>

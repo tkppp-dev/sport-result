d6c857d03ccfcafef8a46a113c857f6931699300
root <taekg.park@gmail.com>
2022-06-11 23:37:46 +0900

    피드백을 위한 로그 추가

--- backend/src/main/kotlin/com/tkppp/sportresult/kbo/controller/KboDayResultController.kt
+++ backend/src/main/kotlin/com/tkppp/sportresult/kbo/controller/KboDayResultController.kt
@@ -3,15 +3,19 @@ package com.tkppp.sportresult.kbo.controller
 import com.tkppp.sportresult.kbo.dto.KboDayMatchRequestDto
 import com.tkppp.sportresult.kbo.dto.KboDayResultResponseDto
 import com.tkppp.sportresult.kbo.service.KboDayResultService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/kbo/day")
 class KboDayResultController(
     private val kboDayResultService: KboDayResultService
 ) {
+
+    private val logger = LoggerFactory.getLogger(KboDayResultController::class.java)
 
     @GetMapping
     fun returnDayResult(): ResponseEntity<List<KboDayResultResponseDto>>{
@@ -22,6 +26,8 @@ class KboDayResultController(
     @PutMapping
     fun updateDayMatch(@RequestBody dayMatchData: List<KboDayMatchRequestDto>): ResponseEntity<Any> {
         kboDayResultService.updateDayMatch(dayMatchData)
+        val now = LocalDateTime.now()
+        logger.info("KBO day match result update success")
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 

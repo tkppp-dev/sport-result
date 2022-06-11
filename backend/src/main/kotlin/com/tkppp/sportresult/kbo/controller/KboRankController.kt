7724654bb31250588a863501523474269fc4645c
root <taekg.park@gmail.com>
2022-06-11 23:38:05 +0900

    임시 로직 삭제

--- backend/src/main/kotlin/com/tkppp/sportresult/kbo/controller/KboRankController.kt
+++ backend/src/main/kotlin/com/tkppp/sportresult/kbo/controller/KboRankController.kt
@@ -1,7 +1,9 @@
 package com.tkppp.sportresult.kbo.controller
 
+import com.tkppp.sportresult.kbo.dto.KboRankDto
 import com.tkppp.sportresult.kbo.dto.KboRankResponseDto
 import com.tkppp.sportresult.kbo.service.KboRankService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -12,6 +14,8 @@ class KboRankController(
     private val kboRankService: KboRankService
 ) {
 
+    private val logger = LoggerFactory.getLogger(KboRankController::class.java)
+
     @GetMapping
     fun getKboRanking(): ResponseEntity<List<KboRankResponseDto>> {
         val body = kboRankService.getKboRankList()
@@ -19,9 +23,9 @@ class KboRankController(
     }
 
     @PutMapping
-    fun updateKboRanking(): ResponseEntity<Any>{
-        val rankData = kboRankService.getKboRankData()
-        rankData?.let { kboRankService.updateKboRank(it) }
+    fun updateKboRanking(@RequestBody kboRankDtos: List<KboRankDto>): ResponseEntity<Any> {
+        kboRankService.updateKboRank(kboRankDtos)
+        logger.info("KBO ranking update success")
 
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }

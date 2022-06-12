bb25fbf35ec1b9f2deb4be43e7251a5f6385224e
root <taekg.park@gmail.com>
2022-06-12 22:54:45 +0900

    매치 날짜를 문자열로 변환해 전달하도록 수정

--- backend/src/main/kotlin/com/tkppp/sportresult/kbo/dto/KboDayResultResponseDto.kt
+++ backend/src/main/kotlin/com/tkppp/sportresult/kbo/dto/KboDayResultResponseDto.kt
@@ -4,17 +4,31 @@ import com.tkppp.sportresult.kbo.domain.KboMatch
 import com.tkppp.sportresult.kbo.util.KboTeam
 import com.tkppp.sportresult.kbo.util.MatchProgress
 import com.tkppp.sportresult.kbo.util.MatchStatus
+import java.time.LocalDate
 import java.time.LocalTime
 
 data class KboDayResultResponseDto(
+    var matchDate: String = "",
     val startTime: LocalTime?,
     val matchStatus: MatchStatus,
     val matchProgress: MatchProgress?,
     val home: String?,
     val away: String?,
+    val homeCode: String?,
+    val awayCode: String?,
     val homeScore: Int,
     val awayScore: Int
 ) {
+
+    private fun getDateString(date: LocalDate): String{
+        val dateString = StringBuilder()
+
+        dateString.append(date.year)
+        dateString.append(if(date.monthValue < 10) "0${date.monthValue}" else "${date.monthValue}")
+        dateString.append(if(date.dayOfMonth < 10) "0${date.dayOfMonth}" else "${date.dayOfMonth}")
+
+        return dateString.toString()
+    }
 
     constructor(entity: KboMatch) : this(
         startTime = entity.startTime,
@@ -22,7 +36,11 @@ data class KboDayResultResponseDto(
         matchProgress = entity.matchProgress,
         home = entity.home?.fullName,
         away = entity.away?.fullName,
+        homeCode = entity.home?.code,
+        awayCode = entity.away?.code,
         homeScore = entity.homeScore,
         awayScore = entity.awayScore
-    )
+    ) {
+        matchDate = getDateString(entity.matchDate)
+    }
 }

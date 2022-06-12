c852c4aae743734476203972fdb7321e8d9e5019
root <taekg.park@gmail.com>
2022-06-12 22:57:03 +0900

    no message

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   outputDir: path.resolve(__dirname, '../backend/src/main/resources/static'),
   devServer: {
-    port: 3000,
+    port: 3001,
     proxy: {
       '/api/*': {
         target: 'http://localhost:8080/'

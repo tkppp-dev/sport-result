c11aa3a26ed8c34c1f653f463f72c4a37374e88b
root <taekg.park@gmail.com>
2022-06-12 22:56:42 +0900

    테스트 추가

--- backend/src/test/kotlin/com/tkppp/sportresult/Integration/KboRankIntegrationTest.kt
+++ backend/src/test/kotlin/com/tkppp/sportresult/Integration/KboRankIntegrationTest.kt
@@ -10,6 +10,7 @@ import com.tkppp.sportresult.kbo.domain.KboRankRepository
 import com.tkppp.sportresult.kbo.dto.KboRankDto
 import com.tkppp.sportresult.kbo.dto.KboRankResponseDto
 import com.tkppp.sportresult.kbo.service.KboRankService
+import com.tkppp.sportresult.kbo.util.KboTeam
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.*
@@ -40,30 +41,46 @@ class KboRankIntegrationTest(
     @Order(1)
     @DisplayName("KBO 랭크 DB 초기화 테스트")
     fun insertKboRank() {
+        // given
+        val rankDtos = listOf(KboRankDto(1, KboTeam.SSG, 10, 8, 0, 2, 0.8f, 0.0f))
+
         // when
-        kboRankController.updateKboRanking()
+        kboRankController.updateKboRanking(rankDtos)
 
         // then
         val rank = kboRankRepository.findAll()
         verify(exactly = 1) { kboRankService.insertKboRank(any()) }
-        rank.map{
-            println("${it.ranking} ${it.name.fullName} ${it.win} ${it.draw} ${it.defeat} ${it.winRate} ${it.gameDiff}")
-        }
+        assertThat(rank[0].ranking).isEqualTo(1)
+        assertThat(rank[0].name).isEqualTo(KboTeam.SSG)
+        assertThat(rank[0].played).isEqualTo(10)
+        assertThat(rank[0].win).isEqualTo(8)
+        assertThat(rank[0].draw).isEqualTo(0)
+        assertThat(rank[0].defeat).isEqualTo(2)
+        assertThat(rank[0].winRate).isEqualTo(0.8f)
+        assertThat(rank[0].gameDiff).isEqualTo(0.0f)
     }
 
     @Test
     @Order(2)
     @DisplayName("KBO 랭크 DB 업데이트 테스트")
     fun updateKboRank() {
+        // given
+        val rankDtos = listOf(KboRankDto(1, KboTeam.LT, 12, 8, 0, 4, 0.75f, 0.0f))
+
         // when
-        kboRankController.updateKboRanking()
+        kboRankController.updateKboRanking(rankDtos)
 
         // then
         val rank = kboRankRepository.findAll()
         verify(exactly = 0) { kboRankService.insertKboRank(any()) }
-        rank.map{
-            println("${it.ranking} ${it.name.fullName} ${it.win} ${it.draw} ${it.defeat} ${it.winRate} ${it.gameDiff}")
-        }
+        assertThat(rank[0].ranking).isEqualTo(1)
+        assertThat(rank[0].name).isEqualTo(KboTeam.LT)
+        assertThat(rank[0].played).isEqualTo(12)
+        assertThat(rank[0].win).isEqualTo(8)
+        assertThat(rank[0].draw).isEqualTo(0)
+        assertThat(rank[0].defeat).isEqualTo(4)
+        assertThat(rank[0].winRate).isEqualTo(0.75f)
+        assertThat(rank[0].gameDiff).isEqualTo(0.0f)
     }
 
     @Test
@@ -79,7 +96,7 @@ class KboRankIntegrationTest(
         val mapper = ObjectMapper().registerKotlinModule()
         val body = mapper.readValue<List<KboRankResponseDto>>(result.response.contentAsString)
 
-        assertThat(body.size).isEqualTo(10)
+        assertThat(body.size).isEqualTo(1)
     }
 
 }

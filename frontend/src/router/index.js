e4ebcbc488d7322ddc31e48eb90cba97436b61c4
root <taekg.park@gmail.com>
2022-06-12 22:57:41 +0900

    HTML Title 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,27 +2,42 @@ import { createRouter, createWebHistory } from "vue-router";
 import Kbo from "../views/Kbo.vue";
 import Nba from '../views/Nba.vue'
 import Lck from '../views/Lck.vue'
+import { nextTick } from "vue";
+
+const baseName = 'Sport Result'
 
 const routes = [
   {
     path: "/",
     name: "Kbo",
     component: Kbo,
+    meta: {
+      title: baseName
+    }
   },
   {
     path: "/kbo",
     name: "Kbo",
     component: Kbo,
+    meta: {
+      title: baseName + ' - KBO'
+    }
   },
   {
     path: "/nba",
     name: "Nba",
     component: Nba,
+    meta: {
+      title: baseName + ' - NBA'
+    }
   }, 
   {
     path: '/lck',
     name: 'Lck',
-    component: Lck
+    component: Lck,
+    meta: {
+      title: baseName + ' - LCK'
+    }
   }
 ];
 
@@ -30,5 +45,12 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+router.afterEach((to) => {
+  const title = to.meta.title ===  undefined ? baseName : to.meta.title
+  nextTick(() => {
+    document.title = title
+  })
+})
 
 export default router;

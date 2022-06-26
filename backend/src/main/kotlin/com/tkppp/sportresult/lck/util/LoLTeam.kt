8da374596a2632e337cc0d4bd078b00dcd15bd8f
root <taekg.park@gmail.com>
2022-06-26 17:00:51 +0900

    fix: Fix wrong name

--- backend/src/main/kotlin/com/tkppp/sportresult/lck/util/LoLTeam.kt
+++ backend/src/main/kotlin/com/tkppp/sportresult/lck/util/LoLTeam.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonValue
 enum class LoLTeam(@JsonValue val fullName: String, val shortName: String) {
     T1("T1", "T1"), GEN("젠지", "젠지"),
     DK("담원 기아", "담원 기아"), DRX("DRX", "DRX"),
-    LSB("리브 샌드박스", "리브 샌박"), KT("KT 롤스터", "KT"),
+    LSB("리브 샌드박스", "리브 샌박"), KT("kt 롤스터", "KT"),
     HLE("한화생명e스포츠", "한화생명"), KDF("광동 프릭스", "광동"),
     NS("농심 레드포스", "농심"), BRO("프레딧 브리온", "프레딧"),
     TBD("TBD", "TBD");

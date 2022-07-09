a7337f3193a250f38a1fe892e1b9340af37a7bb7
root <taekg.park@gmail.com>
2022-07-10 02:31:14 +0900

    refactor: 빌드 outputDir 위치 변경

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../backend/src/main/resources/static'),
+  outputDir: path.resolve(__dirname, '../node-backend/src/public'),
   devServer: {
     port: 3001,
     proxy: {

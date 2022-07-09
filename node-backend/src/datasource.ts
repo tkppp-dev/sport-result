037d78639e3242f7e92485c9b4e6f61c8ebf957d
root <taekg.park@gmail.com>
2022-07-10 02:27:56 +0900

    feat: 환경변수에 따라 datasource 설정하도록 변경

--- node-backend/src/datasource.ts
+++ node-backend/src/datasource.ts
@@ -1,15 +1,31 @@
 import { DataSource } from "typeorm";
 import { KboMatch } from "./domain/kbo/kboMatch";
 import { KboRank } from './domain/kbo/kboRank';
+import { LckMatch } from './domain/lck/lckMatch';
+
+let host, username, password, synchronize, logging
+if(process.env.NODE_ENV === 'production') {
+  host = process.env.DB_HOST
+  username = process.env.DB_USER
+  password = process.env.DB_PASSWORD
+  synchronize = false,
+  logging = false
+} else if (process.env.NODE_ENV === 'development') {
+  host = '127.0.0.1'
+  username = 'root'
+  password = 'root'
+  synchronize = true
+  logging = true
+}
 
 export const MysqlDateSource = new DataSource({
   type: 'mysql',
-  host: '127.0.0.1',
+  host,
   port: 3306,
-  username: 'root',
-  password: 'root',
+  username,
+  password,
   database: 'sport_result',
-  entities: [KboMatch, KboRank],
-  synchronize: true,
-  logging: true
+  entities: [KboMatch, KboRank, LckMatch],
+  synchronize,
+  logging
 })

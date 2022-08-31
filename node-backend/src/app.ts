5eb5df485a126edcc90c9cbe5a5760257c440e7e
root <taekg.park@gmail.com>
2022-08-31 19:10:05 +0900

    refactor: Detach loader and Create loader package

--- node-backend/src/app.ts
+++ node-backend/src/app.ts
@@ -2,12 +2,20 @@ import express, { ErrorRequestHandler } from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-import 'reflect-metadata'
 import 'express-async-errors'
+import 'reflect-metadata'
 
+// loader
+import { config } from 'dotenv'
+config()
+import { bootstrapLogger, getLogger } from './utils/loggers'
+bootstrapLogger()
+import setupLoader from './loaders/loader'
+setupLoader().then(() => {})
+
+// route
 import KboRouter from './domain/kbo/kbo.controller'
 import LckRouter from '@/domain/lck/lck.controller'
-import { getLogger } from './utils/loggers'
 
 class App {
   public app: express.Application
@@ -38,17 +46,24 @@ class App {
       res.sendFile(path.join(__dirname, 'public/index.html'))
     })
 
+    // Internal Server Error
     const errorHandler: ErrorRequestHandler = function (err, req, res, next) {
       const logger = getLogger('MAIN')
-      if (err.status === 404) {
-        res.status(404).json({ code: 404, message: 'Not Found Error' })
-      } else {
-        logger.error(err)
-        res.status(500).json({ code: 500, message: 'Internal Server Error' })
-      }
+      logger.error(err)
+      res.status(500).json({ code: 500, message: 'Internal Server Error' })
     }
     this.app.use(errorHandler)
   }
 }
 
-export default new App().app
+const app = new App().app
+
+const port = process.env.PORT || '8080'
+app.set('port', port)
+
+app.listen(() => {
+  const logger = getLogger('SERVER')
+  logger.info('server launched at port', port)
+})
+
+export default app

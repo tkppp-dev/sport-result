5e0384966d2e8a5982ead8aaa581f8a4784d4384
root <taekg.park@gmail.com>
2022-09-07 23:23:10 +0900

    test: Add helper for test
    - dbConn.ts: orm config file for testing
    - testLoader.ts: configure environment before testing

---
+++ node-backend/src/test/helpers/dbConn.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+import { KboMatch } from "@/domain/kbo/domain/model/kboMatch";
+import { KboRank } from '@/domain/kbo/domain/model/kboRank';
+import { LckMatch } from '@/domain/lck/lckMatch';
+
+export const MysqlDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 9876,
+  username: 'root',
+  password: 'test',
+  database: 'sport_result',
+  entities: [KboMatch, KboRank, LckMatch],
+  timezone: '+09:00',
+  dropSchema: true,
+  synchronize: true,
+  logging: false,
+})

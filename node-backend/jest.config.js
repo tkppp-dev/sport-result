1fd727a6f431708f4d26439756423cc878ac15a1
root <taekg.park@gmail.com>
2022-09-07 23:24:11 +0900

    test: Add configuration file for testing

---
+++ node-backend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.[jt]s?(x)', '**/*.test.[jt]s?(x)'], // 테스트 파일 위치 및 형식
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  setupFiles: ['<rootDir>/src/test/helpers/testLoader.ts'],
+  moduleNameMapper: { '@/(.*)$': '<rootDir>/src/$1' },
+}

127a4edb97b7b9fa7e025f4b4f2ce355e10f1852
root <taekg.park@gmail.com>
2022-09-10 22:36:23 +0900

    fix: Using constructor when making dummy entity for using no datasource environment

--- node-backend/src/test/helpers/createEntity.ts
+++ node-backend/src/test/helpers/createEntity.ts
@@ -1,16 +1,16 @@
 import { MatchProgress, Team } from '@/domain/kbo/domain/model/vo/kbo.vo'
 import { DateUtils } from '@/utils/dateUtils'
 import { KboMatch } from '../../domain/kbo/domain/model/kboMatch'
-import { DatetimeOpts } from '../../utils/dateUtils';
+import { DatetimeOpts } from '../../utils/dateUtils'
 
 export function createKboMatchEntity(datetimeOpts: DatetimeOpts) {
   const { year, month, day, hour, min, sec, ms } = datetimeOpts
-  return KboMatch.create({
-    matchDatetime: DateUtils.getDatetime({ year, month, day, hour, min, sec, ms}),
-    matchProgress: '경기전' as MatchProgress,
-    home: '삼성' as Team,
-    away: 'KIA' as Team,
-    homeScore: 0,
-    awayScore: 0,
-  })
+  const entity = new KboMatch()
+  entity.matchDatetime = DateUtils.getDatetime({ year, month, day, hour, min, sec, ms })
+  entity.matchProgress = '경기전' as MatchProgress
+  entity.home = '삼성' as Team
+  entity.away = 'KIA' as Team
+  entity.homeScore = 0
+  entity.awayScore = 0
+  return entity
 }
